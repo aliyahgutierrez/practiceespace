bb8339b9e8843aac7a78f10aca0c70bf15a2b92b
aliyahgutierrez <gutierrezaliyah@gmail.com>
2022-12-05 12:09:32 -0700

    playing around

---
+++ assets/style.js
@@ -0,0 +1,66 @@
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+    console.log(count)
+
+}
+
+function save(){
+    console.log(count)
+}
+
+// let username = "per"
+
+// let messages = "You have three new notifications" 
+
+// console.log(messages)
+
+// console.log (username + "," + messages + "!")
+
+// let username = "Aliyah"
+
+// let = messages = "you have three new notifications"
+
+// let = messageToUser = username + ", " + messages + "!"
+
+// console.log( messageToUser)
+
+// let username = "Aliyah"
+
+// let greeting = "Hi, my name is " 
+
+
+// let myGreeting = greeting + username 
+
+// console.log(myGreeting)
+
+// let welcomeEL = document.getElementById("welcome-el")
+// console.log(welcomeEL)
+
+// let names = "Aliyah Gutierrez"
+
+// let greeting = " Welcome Back!"
+
+// welcomeEL.innerText = names + greeting
+
+
+// welcomeEL.innerText += "👋"
+
+
+
+
+
+//  DOM DOCUMENT OBJECT MODEL 
+
+// DOCUMTNE BEACAUSE HTML as
+
+//  DATA IS THE OBJECT 
+
+//  THE REAL THING IS
+
+
